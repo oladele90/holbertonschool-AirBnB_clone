a6ec58d45cc756e318f26a635ac9a7582247d485
oladele90 <oladelemiro07@gmail.com>
2023-02-03 10:32:50 -0800

    updated id, created, updated attrs

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """Base class module for AirBnB"""
 import uuid
-import datetime
+from datetime import datetime
 
 class BaseModel:
     """Base class for AirBnB project"""
 
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self, id=None, created_at=0, updated_at=0):
         self.id = uuid.uuid4()
-        self.created_at = datetime.fromtimestamp(time_stamp)
-        self.updated_at = datetime.fromtimestamp(time_stamp)
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
