dde328cd4ad90fe02ee68ca57c8b67aad23ebf51
oladele90 <oladelemiro07@gmail.com>
2023-02-03 08:59:00 -0800

    add self.id attr

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Base class module for AirBnB"""
+import uuid
+
 
 class BaseModel:
     """Base class for AirBnB project"""
 
     def __init__(self, id, created_at, updated_at):
-        
+        self.id = uuid.uuid4()
