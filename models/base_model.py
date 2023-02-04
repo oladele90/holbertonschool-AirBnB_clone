011c967f6425245c9ae7caba71973c6be21b1977
Taypo87 <taylor.poczynek87@gmail.com>
2023-02-04 08:09:00 -0800

    messing with the to_dict method

--- models/base_model.py
+++ models/base_model.py
@@ -2,26 +2,28 @@
 """Base class module for AirBnB"""
 from uuid import uuid4
 from datetime import datetime
-from collections import OrderedDict
+
 class BaseModel:
     """Base class for AirBnB project"""
 
     def __init__(self, id=None, created_at=0, updated_at=0):
         self.id = str(uuid4())
-        self.created_at = str(datetime.utcnow())
-        self.updated_at = str(datetime.utcnow())
+        self.created_at = (datetime.utcnow())
+        self.updated_at = (datetime.utcnow())
 
     def __str__ (self):
         print("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
-        self.updated_at = str(datetime.utcnow())
+        self.updated_at = (datetime.utcnow())
         return self.updated_at
 
     def to_dict(self):
-        dict1 = OrderedDict()
+        dict1 = {}
         dict1['__class__'] = self.__class__.__name__
         for key, item in self.__dict__.items():
-                dict1[key] = item
+            dict1[key] = item
+        dict1['created_at'] = self.created_at.isoformat()
+        dict1['updated_at'] = self.updated_at.isoformat()
         return dict1
