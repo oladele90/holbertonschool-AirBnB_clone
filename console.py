e50f60a07c5608632949c612fdba544409d31a94
Taypo87 <taylor.poczynek87@gmail.com>
2023-02-06 10:51:20 -0800

    console file for for airbnb

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import sys
+import cmd 
+
+class HBNBCommand(cmd.Cmd):
+    """
+        Some sort of comment here
+    """
+    prompt = "(hbnb) "
+
+
+    def do_quit(self, args):
+        """
+        exits console
+        """
+        raise SystemExit
+
+    def do_EOF(self, args):
+        """
+        exits console
+        """
+        raise SystemExit
+
+    def emptyline(self):
+         pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
