507175935d03f38b69793d7c67d45cac6b3a3862
oladele90 <oladelemiro07@gmail.com>
2023-02-07 12:01:20 -0800

    file storsge

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,11 +5,6 @@ from os.path import exists
 from models.base_model import BaseModel
 import models
 from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 
 
 class FileStorage:
