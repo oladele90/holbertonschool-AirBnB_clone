8260283570802bf646b52e5fcbb25482c078f335
oladele90 <oladelemiro07@gmail.com>
2023-02-06 18:53:47 -0800

    tests

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -10,21 +10,32 @@ class Test(unittest.TestCase):
     """creates class of unittests"""
 
     def test_type(self):
+        """test types of object and file path"""
         storage = FileStorage()
         self.assertIsNotNone(storage)
         self.assertIsInstance(storage._FileStorage__objects, dict)
         self.assertEqual(storage._FileStorage__file_path, "file.json")
 
     def test_all(self):
+        """test all function"""
         storage = FileStorage()
         self.assertIsNotNone(storage.all())
 
     def test_new(self):
+        """test new function"""
         storage = FileStorage()
         base1 = BaseModel()
         storage.new(base1)
         key = f'BaseModel.{base1.id}'
         self.assertIsNotNone(storage.all(), {key: base1})
+
+    def test_save():
+        """test save function"""
+        pass
+
+    def test_reload():
+        """test reload function"""
+        pass
 
 
 if __name__ == '__main__':
